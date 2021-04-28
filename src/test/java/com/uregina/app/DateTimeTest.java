7fd729450a17f5aba520f0244e38a8b4f1d4daf2
panli200 <pani200@uregina.ca>
2021-04-27 19:55:13 -0600

    Adding funtion on Datetime test

--- src/test/java/com/uregina/app/DateTimeTest.java
+++ src/test/java/com/uregina/app/DateTimeTest.java
@@ -11,6 +11,20 @@ import org.junit.Test;
 
 public class DateTimeTest {
     @Test
+    public void equalThrowException() throws InvalidDateException, InvalidTimeException {
+        Date d1= new Date(1, 1, 2012);
+        Time12 t1 = new Time12(10, 30, AmPm.am); 
+        DateTime dateTime1 = new DateTime(d1,t1);
+
+        Date d2= new Date(1, 1, 2012);
+        Time12 t2 = new Time12(10, 30, AmPm.am); 
+        DateTime dateTime2 = new DateTime(d2,t2);
+
+        assertEquals(dateTime1, dateTime2);
+    }
+
+
+    @Test
     public void substractCorrect() throws InvalidDateException, InvalidTimeException{
         Date d1= new Date(1, 1, 2012);
         Time12 t1 = new Time12(11, 30, AmPm.am); 
