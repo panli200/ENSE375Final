3418e38d08645adb7c37c67b6ac291ce9e6d8843
panli200 <pani200@uregina.ca>
2021-04-27 18:22:44 -0600

    adding new funtion

--- src/test/java/com/uregina/app/Time12Test.java
+++ src/test/java/com/uregina/app/Time12Test.java
@@ -1,6 +1,6 @@
 package com.uregina.app;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 
 import com.uregina.exceptions.InvalidTimeException;
 
@@ -20,9 +20,20 @@ public class Time12Test {
         try{
             Time12 t1 = new Time12(13, 61, AmPm.am);
             Time12 t2 = new Time12(13, 30, AmPm.am); 
-
             assertEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
 
+        }
+       
+    }
+
+    @Test
+    public void subtractWrongFormat2() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(13, 30, AmPm.pm);
+            Time12 t2 = new Time12(10, 30, AmPm.pm); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
         }
         catch (InvalidTimeException e){
 
