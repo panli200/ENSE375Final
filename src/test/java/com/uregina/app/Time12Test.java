8577861ae4c56432ee49ac3e0089bac80b6ba7e3
panli200 <pani200@uregina.ca>
2021-04-27 18:31:45 -0600

    adding funtions

--- src/test/java/com/uregina/app/Time12Test.java
+++ src/test/java/com/uregina/app/Time12Test.java
@@ -22,10 +22,8 @@ public class Time12Test {
             Time12 t2 = new Time12(13, 30, AmPm.am); 
             assertEquals ( 31, Time12.subtract(t1, t2));
         }
-        catch (InvalidTimeException e){
-
+        catch (InvalidTimeException e){    
         }
-       
     }
 
     @Test
@@ -36,9 +34,116 @@ public class Time12Test {
             assertNotEquals ( 31, Time12.subtract(t1, t2));
         }
         catch (InvalidTimeException e){
-
-        }
-       
+        } 
     }
-    
+    @Test
+    public void subtractWrongFormat3() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(10, 30, AmPm.pm);
+            Time12 t2 = new Time12(13, 30, AmPm.pm); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat4() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(10, 30, AmPm.am);
+            Time12 t2 = new Time12(10, 61, AmPm.pm); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat5() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(10, 61, AmPm.pm);
+            Time12 t2 = new Time12(10, 30, AmPm.am); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat6() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(13, 30, AmPm.am);
+            Time12 t2 = new Time12(10, 61, AmPm.pm); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat7() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(13, 30, AmPm.pm);
+            Time12 t2 = new Time12(13, 30, AmPm.am); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat8() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(13, 30, AmPm.pm);
+            Time12 t2 = new Time12(10, 30, AmPm.pm); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat9() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(13, 61, AmPm.am);
+            Time12 t2 = new Time12(10, 30, AmPm.pm); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat10() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(13, 30, AmPm.am);
+            Time12 t2 = new Time12(13, 61, AmPm.pm); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat11() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(10, 30, AmPm.am);
+            Time12 t2 = new Time12(13, 30, AmPm.pm); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat12() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(10, 61, AmPm.am);
+            Time12 t2 = new Time12(10, 61, AmPm.am); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
+    @Test
+    public void subtractWrongFormat13() throws InvalidTimeException{
+        try{
+            Time12 t1 = new Time12(10, 61, AmPm.am);
+            Time12 t2 = new Time12(13, 30, AmPm.pm); 
+            assertNotEquals ( 31, Time12.subtract(t1, t2));
+        }
+        catch (InvalidTimeException e){
+        } 
+    }
 }
