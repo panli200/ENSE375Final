9cde91785fb030144a94edf0c40f074d842c5b9c
panli200 <pani200@uregina.ca>
2021-04-27 19:46:12 -0600

    adding funtion on file DateTimeTest

--- src/test/java/com/uregina/app/DateTimeTest.java
+++ src/test/java/com/uregina/app/DateTimeTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotEquals;
 
 import com.uregina.exceptions.InvalidDateException;
 import com.uregina.exceptions.InvalidTimeException;
+import com.uregina.exceptions.MoreThanOneDayException;
 
 import org.junit.Test;
 
@@ -15,10 +16,15 @@ public class DateTimeTest {
         Time12 t1 = new Time12(10, 30, AmPm.am); 
         DateTime dateTime1 = new DateTime(d1,t1);
 
-        Date d2= new Date(1, 1, 2012);
+        Date d2= new Date(1, 2, 2012);
         Time12 t2 = new Time12(10, 30, AmPm.am); 
         DateTime dateTime2 = new DateTime(d2,t2);
 
-        assertEquals(dateTime1, dateTime2);
+        try {
+            assertEquals(1440,DateTime.subtract(dateTime1,dateTime2));
+        } catch (MoreThanOneDayException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
